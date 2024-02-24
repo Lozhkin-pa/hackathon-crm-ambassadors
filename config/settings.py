38b90b7053259f5fe59a54f6efeee4c5702b9d9c
Miron Sadykov <reagent992@gmail.com>
2024-02-24 13:45:35 +0300

    Удален глобальный backend filter

--- config/settings.py
+++ config/settings.py
@@ -146,9 +146,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_FILTER_BACKENDS": (
-        "django_filters.rest_framework.DjangoFilterBackend",
-    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
