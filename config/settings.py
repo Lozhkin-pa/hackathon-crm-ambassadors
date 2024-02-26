4ea545c09986a3c810e43ab19b9a61a2d5d72afc
Pavel Lozhkin <123967078+Lozhkin-pa@users.noreply.github.com>
2024-02-26 22:33:32 +0300

    Приложение Контент, модель, админка (#5)
    Приложение Контент:
    - Модель Content.
    - Панель администратора для модели Content.

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,7 @@ LOCAL_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
     "merch.apps.MerchConfig",
+    "content.apps.ContentConfig",
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
