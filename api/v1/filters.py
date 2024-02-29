4b1d4a5252075dac701170de74cb9785c5276b56
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2024-02-29 23:44:25 +0300

    Изменение формата выдачи контента

--- api/v1/filters.py
+++ api/v1/filters.py
@@ -1,10 +1,4 @@
-from django.db.models import Case, Count, When
-from django_filters.rest_framework import (
-    CharFilter,
-    DateFilter,
-    DateFromToRangeFilter,
-    FilterSet,
-)
+from django_filters.rest_framework import DateFilter, FilterSet
 
 from ambassadors.models import Ambassador
 
@@ -26,38 +20,4 @@ class AmbassadorFilter(FilterSet):
             "country",
             "city",
             "created",
-        )
-
-
-class ContentFilter(FilterSet):
-    """
-    Фильтр контента по дате и статусу амбасадора:
-    content/?created_after=2024-02-23&created_before=2024-02-26
-    content/?guide_step=new
-    """
-
-    created = DateFromToRangeFilter()
-    guide_step = CharFilter(method="get_guide_step")
-
-    def get_guide_step(self, queryset, name, value):
-        content = Ambassador.objects.all().annotate(
-            guide_step=Count(Case(When(content__guide=True, then=1)))
-        )
-        match value:
-            case "new":
-                return content.filter(guide_step=0).order_by("-created")
-            case "in_progress":
-                return content.filter(
-                    guide_step__gte=1, guide_step__lte=4
-                ).order_by("-created")
-            case "done":
-                return content.filter(guide_step__gte=4).order_by("-created")
-            case _:
-                return queryset
-
-    class Meta:
-        model = Ambassador
-        fields = (
-            "content__created",
-            "guide_step",
         )
--- api/v1/views/content_view.py
+++ api/v1/views/content_view.py
@@ -1,9 +1,9 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import decorators, filters, status, viewsets
+from django.db.models import Case, Count, When
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import decorators, status, viewsets
 from rest_framework.response import Response
 
 from ambassadors.models import Ambassador
-from api.v1.filters import ContentFilter
 from api.v1.serializers.content_serializer import (
     CreateContentSerializer,
     FormsContentSerializer,
@@ -13,29 +13,86 @@ from api.v1.serializers.content_serializer import (
 from content.models import Content
 
 
+@extend_schema(tags=["Контент"])
+@extend_schema_view(
+    list=extend_schema(
+        summary=("Список амбассадоров с контентом."),
+        description=(
+            "<ul><h3>Фильтрация:</h3>"
+            "<li>Фильтрация по дате: <code>./?created__gte=2023-04-25"
+            "&created__lte=2024-03-25</code>    "
+            "т.е. дата старше 2023-04-25 и младше 2024-03-25</li>"
+            "<li>Фильтрация по статусу гайда: <code>./?guide_step=new   "
+            "т.е. new(новенький)/in_progress(в процессе)/done(выполнено)</li>"
+            "<br><ul><h3>Поиск:</h3>"
+            "<li>Поиск по имени: <code>./?search=Вася</code></li>"
+            "</ul>"
+        ),
+    ),
+    retrieve=extend_schema(summary="Единица контента амбассадора."),
+    create=extend_schema(summary="Создание контента."),
+    partial_update=extend_schema(
+        summary="Редактирование контента амбассадора.",
+    ),
+    forms=extend_schema(summary="Получение контента из Яндекс Формы"),
+)
 class ContentViewSet(viewsets.ModelViewSet):
     """Контент амбассадора."""
 
-    filter_backends = (
-        DjangoFilterBackend,
-        filters.SearchFilter,
-    )
-    # filterset_class = ContentFilter
-    search_fields = ("name",)
-
-    def get_queryset(self):
-        if self.action == "list":
-            self.filterset_class = ContentFilter
-            return Ambassador.objects.all()
-        return Content.objects.all()
+    queryset = Content.objects.all()
 
     def get_serializer_class(self):
-        if self.action == "list":
-            return ListContentSerializer
         if self.action == "retrieve":
             return RetrieveContentSerializer
         if self.action in ["create", "partial_update", "delete"]:
             return CreateContentSerializer
+
+    def list(self, request):
+        guide_step = self.request.query_params.get("guide_step")
+        created_after = self.request.query_params.get("created_after")
+        created_before = self.request.query_params.get("created_before")
+        queryset = Ambassador.objects.all().annotate(
+            guide_step=Count(Case(When(content__guide=True, then=1)))
+        )
+        # Фильтр по дате /?created_after=2024-02-23&created_before=2024-02-26:
+        if created_after and created_before is not None:
+            queryset = queryset.filter(
+                created__range=(created_after, created_before)
+            )
+        serializer_new = ListContentSerializer(
+            queryset.filter(guide_step=0).order_by("-created"), many=True
+        )
+        serializer_in_prog = ListContentSerializer(
+            queryset.filter(guide_step__gte=1, guide_step__lte=4).order_by(
+                "-created"
+            ),
+            many=True,
+        )
+        serializer_done = ListContentSerializer(
+            queryset.filter(guide_step__gte=4).order_by("-created"), many=True
+        )
+        # Фильтр по статусу гайда (/?guide_step=new):
+        match guide_step:
+            case "new":
+                data = serializer_new.data
+            case "in_progress":
+                data = serializer_in_prog.data
+            case "done":
+                data = serializer_done.data
+            case _:
+                data = {
+                    "new": serializer_new.data,
+                    "in_progress": serializer_in_prog.data,
+                    "done": serializer_done.data,
+                }
+        # Поиск по имени (/?name=Смирнова):
+        name = self.request.query_params.get("name")
+        if name is not None:
+            data = ListContentSerializer(
+                queryset.filter(name__icontains=name).order_by("-created"),
+                many=True,
+            ).data
+        return Response(data)
 
     @decorators.action(
         methods=("post",),
