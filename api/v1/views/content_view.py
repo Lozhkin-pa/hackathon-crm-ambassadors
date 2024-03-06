2488b848fd3abd53c7507e92e1fb6787d0a63adb
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2024-03-06 14:48:14 +0300

    Добавлено поле Направление

--- api/v1/views/content_view.py
+++ api/v1/views/content_view.py
@@ -53,18 +53,18 @@ class ContentViewSet(viewsets.ModelViewSet):
         Получение списка амбассадоров с контентом.
         Фильтрация по статусу гайда (/?guide_step=new).
         Фильтр по дате (/?created_after=2024-02-23&created_before=2024-02-26).
-        Поиск по имени (/?name=Смирнова).
+        Поиск по имени (/?search=Смирнова).
         """
         guide_step = self.request.query_params.get("guide_step")
         created_after = self.request.query_params.get("created_after")
         created_before = self.request.query_params.get("created_before")
-        name = self.request.query_params.get("name")
+        search = self.request.query_params.get("search")
         queryset = Ambassador.objects.all().annotate(
             guide_step=Count(Case(When(content__guide=True, then=1)))
         )
         # Поиск по имени:
-        if name:
-            queryset = queryset.filter(name__icontains=name)
+        if search:
+            queryset = queryset.filter(name__icontains=search)
         # Фильтрация по дате:
         if created_after:
             queryset = queryset.filter(created__gte=created_after)
